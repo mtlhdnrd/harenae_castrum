9e47fd82ecf1b87ec8ca6971fcf9ec98f55465d6
0pia <bako.borka2005@tanulo.boronkay.hu>
2024-02-29 12:17:33 +0100

    davyfv

--- site/script/jump_animation.js
+++ site/script/jump_animation.js
@@ -1,45 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
     const arrowDown = document.querySelector('.arrow-down');
     const arrowUp = document.querySelector('.arrow-up');
-    const footer = document.querySelector('footer');
 
     arrowDown.addEventListener('click', function() {
-        const isFooterVisible = footer.getBoundingClientRect().top < window.innerHeight;
+        jumpTo('bottom');
+    });
 
-        if (isFooterVisible) {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        } else {
+    arrowUp.addEventListener('click', function() {
+        jumpTo('top');
+    });
+
+    // Function to scroll or jump to a specified position
+    function jumpTo(destination) {
+        if (destination === 'bottom') {
             window.scrollTo({
                 top: document.body.scrollHeight,
                 behavior: 'smooth'
             });
+            arrowDown.style.display = 'none';
+            arrowUp.style.display = 'block';
+        } else if (destination === 'top') {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+            arrowUp.style.display = 'none';
+            arrowDown.style.display = 'block';
         }
-    });
-
-    arrowUp.addEventListener('click', function() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-
-    // Function to rotate the arrow icon
-    function rotateArrow(degrees) {
-        arrowDown.style.display = degrees === 0 ? 'block' : 'none';
-        arrowUp.style.display = degrees === 180 ? 'block' : 'none';
     }
 
     // Event listener to detect scrolling
     window.addEventListener('scroll', function() {
-        const isFooterVisible = footer.getBoundingClientRect().top < window.innerHeight;
+        const isAtTop = window.scrollY === 0;
+        const isAtBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight;
 
-        if (isFooterVisible) {
-            rotateArrow(180); // Rotate the arrow 180 degrees when at the bottom
+        if (isAtBottom) {
+            arrowDown.style.display = 'none';
+            arrowUp.style.display = 'block';
+        } else if (isAtTop) {
+            arrowUp.style.display = 'none';
+            arrowDown.style.display = 'block';
         } else {
-            rotateArrow(0); // Rotate the arrow back to its original position when not at the bottom
+            arrowDown.style.display = 'block';
+            arrowUp.style.display = 'block';
         }
     });
 });
+
+function openOverlay() {
+    document.getElementById('overlay').style.display = 'block';
+  }
+  
+  function closeOverlay() {
+    document.getElementById('overlay').style.display = 'none';
+  }
+  
