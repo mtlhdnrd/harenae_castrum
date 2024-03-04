ef1c11a4ec6fdb0cd3123f2bf83309106781199a
0pia <bako.borka2005@tanulo.boronkay.hu>
2024-03-04 19:17:08 +0100

    asom a helyemet mert megoltok idegileg a sirba teszTEEEEEEK

--- site/script/overlay.js
+++
@@ -1,7 +0,0 @@
-function openOverlay() {
-    document.getElementById('overlay').style.display = 'block';
-}
-
-function closeOverlay() {
-    document.getElementById('overlay').style.display = 'none';
-}
