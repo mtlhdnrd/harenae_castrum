6f57b49fdadd61e2759d8c1e73869203cf7ee803
mtlhdnrd <mtlhdnrd@protonmail.com>
2024-03-05 08:37:05 +0100

    commit a69cc6e makes this obsolete, i can just query it now

--- site/api/calculate_price.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function calculate_price($hostility, $landable, $participants) {
-        $price = $hostility * $participants;
-        if($landable == 0) {
-            $price *= 1.2;
-        }
-        return $price;
-    }
