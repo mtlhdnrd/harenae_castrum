0ce377c4e477a4a905d5706ea5a011dfe1b3b5f9
mtlhdnrd <mtlhdnrd@protonmail.com>
2024-03-05 10:12:20 +0100

    some cleanup in product.php and style.css, fixed another id issue

--- site/product.php
+++ site/product.php
@@ -34,54 +34,25 @@
         </header>
         <main class="product_main">
             <?php
-                //require_once "./api/classes.php";
                 ob_start();
                 include_once "./api/get_planets.php";
                 $planets = ob_get_clean();
-                //echo $planets."<br>";
-                //for($i = 0; $i < 8; $i++) {
                 foreach(json_decode($planets) as $planet) {
                     echo "<div id='overlay".$planet->ID."' class='overlay' onclick='closeOverlay(".$planet->ID.")'>";
                     echo "    <img src='./img/planets/".$planet->infopanel."'>";
                     echo "</div>";
                     echo "<div class='product_item'>";
                     echo "    <img src='./img/planets/".$planet->image."' onclick='openOverlay(".$planet->ID.")'>";
-                    echo "    <div class='product_desc' id='details'>";
+                    echo "    <div class='product_desc details'>";
                     echo "        <h2>".$planet->name."</h2>";
                     echo "        <p>".$planet->description."</p>";
                     echo "    </div>";
-                    echo "    <div class='product_desc add'>";
+                    echo "    <form class='product_desc add'>";
                     echo "        <a class='termek_button' href=''>Foglalás</a>";
-                    echo "    </div>";
+                    echo "    </form>";
                     echo "</div>";
                 }
             ?>
-            <!--div id="overlay10" class="overlay" onclick="closeOverlay(10)">
-                <img src="./img/boylgo.jpg">
-            </div>
-            <div class="product_item">
-                <img src="./img/boylgo.jpg" onclick="openOverlay(10)">
-                <div class="product_desc" id="details">
-                    <h2>Planet_Name</h2>
-                    <p>efhafoiahndfihníaifvdijvni íabvuiadíbvuiabdvlibdavilbdagv eibdblvíakegbvilbígriwíb</p>
-                </div>
-                <div class="product_desc add">
-                    <a class="termek_button" href="">Foglalás</a>
-                </div>
-            </div>
-            <div id="overlay20" class="overlay" onclick="closeOverlay(20)">
-                <img src="./img/planets/crystallis_infopanel_00000.png">
-            </div>
-            <div class="product_item">
-                <img src="./img/planets/crystallis_min_00000.png" onclick="openOverlay(20)">
-                <div class="product_desc" id="details">
-                    <h2>Crystallis</h2>
-                    <p>Egy bolyó-méretű szellemváros, amely arra készteti az egyént, hogy újragondolja a háború fogalmát, miközben az áldozatok rideg, drágakövekbe fagyott tekintetükkel vizslatják a betolakodót.</p>
-                </div>
-                <div class="product_desc add">
-                    <a class="termek_button" href="">Foglalás</a>
-                </div>
-            </div-->
         </main>
     </body>
 </html>
